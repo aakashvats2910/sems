6755f4b934826e500838f38c5b29f1812d38e66b
Aakash Sharma <aakashvats2910@gmail.com>
2020-03-04 12:29:04 +0530

    Changed backend to common google account

--- js/auth.js
+++ js/auth.js
@@ -1,26 +1,29 @@
-const register = document.querySelector('.login-content');
-		register.addEventListener('submit', (e) =>{
+const register = document.querySelector('#sign-up');
+const form = document.querySelector('#form');
+		register.addEventListener('click', (e) =>{
 			e.preventDefault();
 			const username = document.querySelector('#username');
 			const pass = document.querySelector('#pass');
 			const roll = document.querySelector('#roll');
 			const mess = document.querySelector('#mess');
 
+			var firebase = require('firebase');
+var firebaseui = require('firebaseui');
+
 			auth.createUserWithEmailAndPassword(username.value, pass.value).then(c => {
 				
 				db.collection("students").doc(c.user.uid).set({
-				rollnuber: roll.value	    			
-			})
-			.then(() => {
-				username.innerHTML = '';
-				pass.innerHTML = '';
-				roll.innerHTML = '';
-			})
-			.catch(function(error) {
-				console.error("Error writing document: ", error);
-			});
+					rollnumber: roll.value,
+					password: pass.value
+				})
+
+			console.log("successfull");
+			form.reset();
 
 
+			}).catch((err) =>{
+				console.log("errorororor", err.message);
+				form.reset();
 			})
 
 		});
