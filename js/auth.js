c1bc01d13a2742f483c559121f32198fd16cba6c
Aakash Sharma <aakashvats2910@gmail.com>
2020-03-21 19:55:51 +0530

    Second deploy

--- js/auth.js
+++ js/auth.js
@@ -5,18 +5,20 @@ const form = document.querySelector('#form');
 			const username = document.querySelector('#username');
 			const pass = document.querySelector('#pass');
 			const roll = document.querySelector('#roll');
+			const mobile = document.querySelector('#mobile');
+			const form = document.querySelector('#form');
 			const mess = document.querySelector('#mess');
 
 			auth.createUserWithEmailAndPassword(username.value, pass.value).then(c => {
 				
 				db.collection("students").doc(c.user.uid).set({
 					rollnumber: roll.value,
-					password: pass.value
+					password: pass.value,
+					mobile: mobile.value
 				})
 
 			console.log("successfull");
 			form.reset();
-
 
 			}).catch((err) =>{
 				console.log("errorororor", err.message);
