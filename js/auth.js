585e0f350382052eef638711365770ec6d2568b0
Aakash Sharma <aakashvats2910@gmail.com>
2020-03-22 18:30:50 +0530

    OTP working

--- js/auth.js
+++ js/auth.js
@@ -9,21 +9,49 @@ const form = document.querySelector('#form');
 			const form = document.querySelector('#form');
 			const mess = document.querySelector('#mess');
 
-			auth.createUserWithEmailAndPassword(username.value, pass.value).then(c => {
+			window.recaptchaVerifier = new firebase.auth.RecaptchaVerifier('recaptcha-container-new');
+			var appVerifier = window.recaptchaVerifier;
+
+			console.log(pass.value);
+
+			firebase.auth().signInWithPhoneNumber("+91" + mobile.value, appVerifier)
+				.then(function (confirmationResult) {
+					console.log("SMS SENT! HUrray!");
+					window.open('OTP.html','_self');
+					console.log('OPENED!!!');
+
+					sessionStorage.setItem('username_m', username.value);
+					sessionStorage.setItem('roll_m', roll.value);
+					sessionStorage.setItem('pass_m', pass.value);
+					sessionStorage.setItem('mobile_m', mobile.value);
+
+				// SMS sent. Prompt user to type the code from the message, then sign the
+				// user in with confirmationResult.confirm(code).
+				window.confirmationResult = confirmationResult;
+				}).catch(function (error) {
+					console.log("======");
+					console.log(error.message);
+				// Error; SMS not sent
+				// ...
+				});
+
+			// auth.createUserWithEmailAndPassword(username.value, pass.value).then(c => {
 				
-				db.collection("students").doc(c.user.uid).set({
-					rollnumber: roll.value,
-					password: pass.value,
-					mobile: mobile.value
-				})
+			// 	db.collection("students").doc(c.user.uid).set({
+			// 		rollnumber: roll.value,
+			// 		password: pass.value,
+			// 		mobile: mobile.value
+			// 	})
 
-			console.log("successfull");
-			form.reset();
+			// console.log("successfull");
+			// form.reset();
 
-			}).catch((err) =>{
-				console.log("errorororor", err.message);
-				form.reset();
-			})
+			// }).catch((err) =>{
+			// 	console.log("errorororor", err.message);
+			// 	form.reset();
+			// })
 
 		});
+
+
         
