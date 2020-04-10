a9b914bc5193548fc201e9484aac2592fb33d919
Aakash Sharma <aakashvats2910@gmail.com>
2020-04-10 18:18:42 +0530

    Modified student/admin login

--- js/dashboard.js
+++ js/dashboard.js
@@ -1,15 +1,20 @@
 const students_field = document.querySelector('#students-registered');
 const teacher_field = document.querySelector('#teacher-available');
 const ongoing_field = document.querySelector('#ongoing-events');
+const assigned_field = document.querySelector('#teacher-assigned');
 const admin_name = document.querySelector('#admin-name');
 const event_submit_button = document.querySelector('#event-submit-button');
 const log_out_button = document.querySelector('#log-out-button');
 
+// For the submission of the event on the new Dialog Box!
 const event_name_field = document.querySelector('#event-name-field');
 const description_field = document.querySelector('#description-field');
 const rules_regulations_field = document.querySelector('#rules-regulation-field');
 const prize_field = document.querySelector('#prize-field');
 const teachers_assigned_field = document.querySelector('#teachers-assigned-field');
+
+// For the total events!
+const events_list_field = document.querySelector('#events-list');
 
 const right_log_out = document.querySelector('#right-log-out');
 
@@ -28,7 +33,7 @@ if (sessionStorage.getItem('uid') == null) {
 }
 
 // Initiate the dashboard with the basic data.
-getBasicData(sessionStorage.getItem('uid'));
+getBasicDataAdmin(sessionStorage.getItem('uid'));
 
 event_submit_button.addEventListener('click', (e) => {
     console.log('EVENT SUBMIT BUTTON');
@@ -44,21 +49,22 @@ right_log_out.addEventListener('click', (e) => {
     console.log('LOg ouT');
 });
 
-function getBasicData(id) { 
-    // ,------.,--.,------. ,------.    ,--.   ,--. ,-----. ,------. ,--. ,--. ,---.   
-    // |  .---'|  ||  .--. '|  .---'    |  |   |  |'  .-.  '|  .--. '|  .'   /'   .-'  
-    // |  `--, |  ||  '--'.'|  `--,     |  |.'.|  ||  | |  ||  '--'.'|  .   ' `.  `-.  
-    // |  |`   |  ||  |\  \ |  `---.    |   ,'.   |'  '-'  '|  |\  \ |  |\   \.-'    | 
-    // `--'    `--'`--' '--'`------'    '--'   '--' `-----' `--' '--'`--' '--'`-----'  
-                                                                                
+// ,------.,--.,------. ,------.    ,--.   ,--. ,-----. ,------. ,--. ,--. ,---.   
+// |  .---'|  ||  .--. '|  .---'    |  |   |  |'  .-.  '|  .--. '|  .'   /'   .-'  
+// |  `--, |  ||  '--'.'|  `--,     |  |.'.|  ||  | |  ||  '--'.'|  .   ' `.  `-.  
+// |  |`   |  ||  |\  \ |  `---.    |   ,'.   |'  '-'  '|  |\  \ |  |\   \.-'    | 
+// `--'    `--'`--' '--'`------'    '--'   '--' `-----' `--' '--'`--' '--'`-----'  
+
+function getBasicDataAdmin(id) {
     db.collection('admin').doc(id).get().then(function (doc) {
         if (doc.exists) {
             var adminData = doc.data();
             admin_name.innerText = adminData.NAME;
         } else {
-            console.log('Unable to get data!');
-            window.alert('No legitimate User found!');
-            window.open('login.html','_self');
+            // console.log('Unable to get data!');
+            // window.alert('No legitimate User found!');
+            // window.open('login.html', '_self');
+            getBasicDataStudent(id);
         }
     });
     db.collection('staticdata').doc('Y2Jb231xdez5eLcEFLKt').get().then(function (doc) {
@@ -67,12 +73,28 @@ function getBasicData(id) {
             students_field.innerText = staticData.STUDENTS_REGISTERED;
             teacher_field.innerText = staticData.TEACHERS_AVAILABLE;
             ongoing_field.innerText = staticData.ONGOING_EVENTS;
+            assigned_field.innerText = staticData.TEACHERS_ASSIGNED;
         } else {
             console.log('Nothing found!');
         }
     }).catch(function (error) {
         console.log('Error :: ' + error.message);
-    }); 
+    });
+    initiateEvents();
+}
+
+// Alpha vserion.
+function getBasicDataStudent(id) {
+    db.collection('students').doc(id).get().then(function (doc) {
+        if (doc.exists) {
+            var adminData = doc.data();
+            admin_name.innerText = adminData.NAME;
+        } else {
+            console.log('Unable to get data!');
+            window.alert('No legitimate User found!');
+            window.open('login.html', '_self');
+        }
+    });
 }
 
 // Under development.
@@ -86,7 +108,7 @@ function registerEvent(eventsName, description, rulesRegulations, prizes, teache
     }).then(ref => {
         console.log('Auto ID : ' + ref.id);
         window.alert('Event Registered!');
-    }).catch(function(error) {
+    }).catch(function (error) {
         console.log('Unwated error :: ' + error.message);
     });
 }
@@ -94,10 +116,69 @@ function registerEvent(eventsName, description, rulesRegulations, prizes, teache
 function logOut() {
     firebase.auth().signOut().then(function () {
         console.log('signed_out-successfully');
-    }).then(function() {
+    }).then(function () {
         sessionStorage.setItem('uid', null);
-        window.open('login.html','_self');
+        window.open('login.html', '_self');
     }).catch(function (error) {
         console.log('===== : ' + error.message);
     });
 }
+
+function initiateEvents() {
+    db.collection("events").get().then(function (querySnapshot) {
+        querySnapshot.forEach(function (doc) {
+            var abc = doc.data();
+            addToEventsList(abc.EVENT_NAME);
+        })
+    });
+}
+
+function addToEventsList(item_) {
+    var node = document.createElement('li');
+    var textNode = document.createTextNode(item_);
+    var a = document.createElement('a');
+    a.setAttribute('href', 'https://www.google.com');
+    a.appendChild(textNode);
+    node.appendChild(a);
+    events_list_field.appendChild(node);
+}
+
+// _______ _                     _                                    _       _   _     _                           _                      _ 
+// |__   __| |                   (_)                                  | |     | | | |   (_)                         | |                    | |
+//    | |  | |__   ___ _ __ ___   _ ___   _ __   ___    ___ _   _  ___| |__   | |_| |__  _ _ __   __ _    __ _ ___  | |__   ___  _ __   ___| |
+//    | |  | '_ \ / _ \ '__/ _ \ | / __| | '_ \ / _ \  / __| | | |/ __| '_ \  | __| '_ \| | '_ \ / _` |  / _` / __| | '_ \ / _ \| '_ \ / _ \ |
+//    | |  | | | |  __/ | |  __/ | \__ \ | | | | (_) | \__ \ |_| | (__| | | | | |_| | | | | | | | (_| | | (_| \__ \ | | | | (_) | |_) |  __/_|
+//    |_|  |_| |_|\___|_|  \___| |_|___/ |_| |_|\___/  |___/\__,_|\___|_| |_|  \__|_| |_|_|_| |_|\__, |  \__,_|___/ |_| |_|\___/| .__/ \___(_)
+//                                                                                                __/ |                         | |           
+//                                                                                               |___/                          |_|           
+// _______ _                                           _            __   _                        _                   _   _     _             
+// |__   __| |                                         | |          / _| | |                      (_)                 | | | |   (_)            
+//    | |  | |__   ___    ___ ___  _ __   ___ ___ _ __ | |_    ___ | |_  | |__   ___  _ __   ___   _ ___   _ __   ___ | |_| |__  _ _ __   __ _ 
+//    | |  | '_ \ / _ \  / __/ _ \| '_ \ / __/ _ \ '_ \| __|  / _ \|  _| | '_ \ / _ \| '_ \ / _ \ | / __| | '_ \ / _ \| __| '_ \| | '_ \ / _` |
+//    | |  | | | |  __/ | (_| (_) | | | | (_|  __/ |_) | |_  | (_) | |   | | | | (_) | |_) |  __/ | \__ \ | | | | (_) | |_| | | | | | | | (_| |
+//    |_|  |_| |_|\___|  \___\___/|_| |_|\___\___| .__/ \__|  \___/|_|   |_| |_|\___/| .__/ \___| |_|___/ |_| |_|\___/ \__|_| |_|_|_| |_|\__, |
+//                                               | |                                 | |                                                  __/ |
+//                                               |_|                                 |_|                                                 |___/ 
+// _   _                         _       _                                                            _  
+// | | | |                       (_)     (_)                                                          | | 
+// | |_| |__   __ _ _ __     __ _ ___   ___ _ __   __ _   _   _ _ __      __ _  __      _____  _ __ __| | 
+// | __| '_ \ / _` | '_ \   / _` | \ \ / / | '_ \ / _` | | | | | '_ \    / _` | \ \ /\ / / _ \| '__/ _` | 
+// | |_| | | | (_| | | | | | (_| | |\ V /| | | | | (_| | | |_| | |_) |  | (_| |  \ V  V / (_) | | | (_| | 
+//  \__|_| |_|\__,_|_| |_|  \__, |_| \_/ |_|_| |_|\__, |  \__,_| .__( )  \__,_|   \_/\_/ \___/|_|  \__,_| 
+//                           __/ |                 __/ |       | |  |/                                    
+//                          |___/                 |___/        |_|                                        
+// _   _           _     _           _     _                     _                                                _               
+// | | | |         | |   | |         | |   | |                   | |                                              (_)              
+// | |_| |__   __ _| |_  | |__   ___ | | __| |___   _ __   ___   | |_ _ __ _   _  ___   _ __ ___   ___  __ _ _ __  _ _ __   __ _   
+// | __| '_ \ / _` | __| | '_ \ / _ \| |/ _` / __| | '_ \ / _ \  | __| '__| | | |/ _ \ | '_ ` _ \ / _ \/ _` | '_ \| | '_ \ / _` |  
+// | |_| | | | (_| | |_  | | | | (_) | | (_| \__ \ | | | | (_) | | |_| |  | |_| |  __/ | | | | | |  __/ (_| | | | | | | | | (_| |_ 
+//  \__|_| |_|\__,_|\__| |_| |_|\___/|_|\__,_|___/ |_| |_|\___/   \__|_|   \__,_|\___| |_| |_| |_|\___|\__,_|_| |_|_|_| |_|\__, (_)
+//                                                                                                                          __/ |  
+//                                                                                                                         |___/   
+//             ____  _     _ _          _    _      _     _ _           
+//            / __ \| |   (_) |        | |  | |    | |   (_) |          
+//   ______  | |  | | |__  _| |_ ___   | |  | | ___| |__  _| |__   __ _ 
+//  |______| | |  | | '_ \| | __/ _ \  | |  | |/ __| '_ \| | '_ \ / _` |
+//           | |__| | |_) | | || (_) | | |__| | (__| | | | | | | | (_| |
+//            \____/|_.__/|_|\__\___/   \____/ \___|_| |_|_|_| |_|\__,_|
+
